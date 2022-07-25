93657a2a1ad6c10c39be17a615ef8529e117c75b
Lizan <lizan@Lizans-Macbook-Pro-2.local>
2022-07-25 15:45:24 +0545

    caredit cards listing

---
+++ Sharemil/Modules/PaymentOptions/View/Cell/PaymentOptionsListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PaymentOptionsListTableViewCell.swift
+//  Sharemil
+//
+//  Created by Lizan on 25/07/2022.
+//
+
+import UIKit
+
+class PaymentOptionsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
