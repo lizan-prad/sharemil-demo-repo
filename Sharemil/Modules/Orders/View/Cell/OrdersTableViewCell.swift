1a8f0e14dc6c94a2840afc663c8d9a9fa52cd868
Lizan <lizan.pra19@gmail.com>
2022-11-03 00:04:10 +0545

    bug fixes

--- Sharemil/Modules/Orders/View/Cell/OrdersTableViewCell.swift
+++ Sharemil/Modules/Orders/View/Cell/OrdersTableViewCell.swift
@@ -33,7 +33,7 @@ class OrdersTableViewCell: UITableViewCell {
             formatter.dateFormat = "MMM dd"
             self.dateStatusLabel.text = "\(formatter.string(from: orderDate ?? Date())) · \(model?.status ?? "")"
             formatter.dateFormat = "hh:mm a"
-            self.itemsPriceLabel.text = "\(count) \(count == 1 ? "item" : "items")" + (model?.status?.lowercased() == "completed" ? "" : ("· $" + (formatter.string(from: time ?? Date()))))
+            self.itemsPriceLabel.text = "\(count) \(count == 1 ? "item" : "items")" + (model?.status?.lowercased() == "completed" ? "" : (" · " + (formatter.string(from: time ?? Date()))))
         }
     }
 
