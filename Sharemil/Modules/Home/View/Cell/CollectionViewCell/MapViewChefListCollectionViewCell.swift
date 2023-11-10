373182df0c8d76230d769f18045b1712f2c2548b
Lizan <lizan@Lizans-MacBook-Pro.local>
2023-11-10 23:53:10 +0545

    icon changed

--- Sharemil/Modules/Home/View/Cell/CollectionViewCell/MapViewChefListCollectionViewCell.swift
+++ Sharemil/Modules/Home/View/Cell/CollectionViewCell/MapViewChefListCollectionViewCell.swift
@@ -10,6 +10,9 @@ import SDWebImage
 
 class MapViewChefListCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var closedView: UIView!
+    @IBOutlet weak var offerBox: UIView!
+    @IBOutlet weak var offerText: UILabel!
     @IBOutlet weak var container: UIView!
     @IBOutlet weak var chefImage: UIImageView!
     @IBOutlet weak var openingHour: UILabel!
@@ -17,6 +20,9 @@ class MapViewChefListCollectionViewCell: UICollectionViewCell {
     
     var model: ChefListModel? {
         didSet {
+            self.offerBox.isHidden = model?.note == nil || model?.note == ""
+            self.offerText.text = model?.note
+            closedView.rounded()
             chefImage.sd_setImage(with: URL.init(string: model?.mainImageUrl ?? ""))
             chefName.text = "\(model?.firsName ?? "") \(model?.lastName ?? "")"
             let formatter = DateFormatter()
@@ -25,6 +31,13 @@ class MapViewChefListCollectionViewCell: UICollectionViewCell {
             let hour = model?.hours?.filter({($0.day?.lowercased() ?? "") == now.prefix(3)}).first
             formatter.dateFormat = "HH:mm:ss"
             let date = formatter.date(from: hour?.startTime ?? "")
+            let nowStrDate = formatter.string(from: Date())
+            let nowDate = formatter.date(from: nowStrDate)
+            let d = formatter.date(from: hour?.endTime ?? "")
+            let sdate = formatter.date(from: hour?.startTime ?? "")
+            self.chefImage.alpha = (model?.isOpen == true && ((sdate ?? Date())...(d ?? Date())).contains(nowDate ?? Date())) ? 1 : 0.5
+            closedView.isHidden = (model?.isOpen == true && ((sdate ?? Date())...(d ?? Date())).contains(nowDate ?? Date()))
+            
             formatter.dateFormat = "hh:mm a"
             self.openingHour.text = "Opens at \(formatter.string(from: date ?? Date()))"
         }
